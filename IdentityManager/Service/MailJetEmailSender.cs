4d7bd88ba2f60ef8d4025b7ae4edeefdb02ee8a9
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-08-30 15:34:03 -0500

    Section 3 - Setup MailJet Email Sender

---
+++ IdentityManager/Service/MailJetEmailSender.cs
@@ -0,0 +1,81 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityManager.Service
+{
+    public class MailJetEmailSender : IEmailSender
+    {
+        private readonly IConfiguration _configuration;
+        public MailJetOptions _mailJetOptions;
+        public MailJetEmailSender(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        public async Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+
+            _mailJetOptions = _configuration.GetSection("MailJet").Get<MailJetOptions>();
+
+            MailjetClient client = new MailjetClient(_mailJetOptions.ApiKey, _mailJetOptions.SecretKey)
+            {
+                Version = ApiVersion.V3_1,
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+             new JObject {
+      {
+       "From",
+       new JObject {
+        {"Email", "benspark01@protonmail.com"},
+        {"Name", "Ben"}
+       }
+      }, {
+       "To",
+       new JArray {
+        new JObject {
+         {
+          "Email",
+          "benspark01@protonmail.com"
+         }, {
+          "Name",
+          "Ben"
+         }
+        }
+       }
+      }, {
+       "Subject",
+      subject
+      },  {
+       "HTMLPart",
+       htmlMessage
+      },
+     }
+             });
+            await client.PostAsync(request);
+            //if (response.IsSuccessStatusCode)
+            //{
+            //    Console.WriteLine(string.Format("Total: {0}, Count: {1}\n", response.GetTotal(), response.GetCount()));
+            //    Console.WriteLine(response.GetData());
+            //}
+            //else
+            //{
+            //    Console.WriteLine(string.Format("StatusCode: {0}\n", response.StatusCode));
+            //    Console.WriteLine(string.Format("ErrorInfo: {0}\n", response.GetErrorInfo()));
+            //    Console.WriteLine(response.GetData());
+            //    Console.WriteLine(string.Format("ErrorMessage: {0}\n", response.GetErrorMessage()));
+            //}
+        }
+    }
+}
+  
+
