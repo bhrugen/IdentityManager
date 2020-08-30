c858532def315b92c37fd6e70f075c50f72dbede
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-08-30 11:19:34 -0500

    Section 1 - Create Database

---
+++ IdentityManager/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityManager.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions options): base(options)
+        {
+
+        }
+    }
+}
