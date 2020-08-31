3205b6ffd66a8a8b982b320a4f89206be88d88e1
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-08-30 19:13:06 -0500

    Section 3 - Demo Confrim Email

--- IdentityManager/Controllers/AccountController.cs
+++ IdentityManager/Controllers/AccountController.cs
@@ -63,6 +63,23 @@ namespace IdentityManager.Controllers
         }
 
         [HttpGet]
+        public async Task<IActionResult> ConfirmEmail(string userId,string code)
+        {
+            if(userId==null || code == null)
+            {
+                return View("Error");
+            }
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+            {
+                return View("Error");
+            }
+            var result = await _userManager.ConfirmEmailAsync(user, code);
+            return View(result.Succeeded ? "ConfirmEmail" : "Error");
+
+        }
+
+        [HttpGet]
         public IActionResult Login(string returnurl=null)
         {
             ViewData["ReturnUrl"] = returnurl;
