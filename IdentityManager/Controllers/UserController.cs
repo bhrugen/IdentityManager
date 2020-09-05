615b17600c7ff4955adcbaadf212647e251f1e4b
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-09-05 13:11:29 -0500

    Section 7 - User Controller Index Action

---
+++ IdentityManager/Controllers/UserController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IdentityManager.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityManager.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UserController(ApplicationDbContext db, UserManager<IdentityUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var userList = _db.ApplicationUser.ToList();
+            var userRole = _db.UserRoles.ToList();
+            var roles = _db.Roles.ToList();
+            foreach(var user in userList)
+            {
+                var role = userRole.FirstOrDefault(u => u.UserId == user.Id);
+                if (role == null)
+                {
+                    user.Role = "None";
+                }
+                else
+                {
+                    user.Role = roles.FirstOrDefault(u => u.Id == role.RoleId).Name;
+                }
+            }
+
+            return View(userList);
+
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- IdentityManager/Models/ApplicationUser.cs
+++ IdentityManager/Models/ApplicationUser.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,5 +12,10 @@ namespace IdentityManager.Models
     {
         [Required]
         public string Name { get; set; }
+        [NotMapped]
+        public string RoleId { get; set; }
+        [NotMapped]
+        public string Role{ get; set; }
+
     }
 }
