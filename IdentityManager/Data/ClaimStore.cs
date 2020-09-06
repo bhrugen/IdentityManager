4ce7edec82c09cbe734211e417c2b56dd819497e
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-09-06 16:18:47 -0500

    Section 9 - Create Claims Store and View Model

---
+++ IdentityManager/Data/ClaimStore.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace IdentityManager.Data
+{
+    public static class ClaimStore
+    {
+        public static List<Claim> claimsList = new List<Claim>()
+        {
+            new Claim("Create","Create"),
+            new Claim("Edit","Edit"),
+            new Claim("Delete","Delete")
+        };
+    }
+}
---
+++ IdentityManager/Models/UserClaimsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityManager.Models
+{
+    public class UserClaimsViewModel
+    {
+        public UserClaimsViewModel()
+        {
+            Claims = new List<UserClaim>();
+        }
+        public string UserId { get; set; }
+        public List<UserClaim> Claims { get; set; }
+    }
+
+    public class UserClaim
+    {
+        public string ClaimType { get; set; }
+        public bool IsSelected { get; set; }
+    }
+}
