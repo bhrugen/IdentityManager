8bfb00704fe93c6ccb25d85aa688b32000277cf1
Bhrugen Patel <bhrugen1990@hotmail.com>
2020-09-06 23:27:36 -0500

    Section 10 - Allow Anonymous and Authorize

--- IdentityManager/Controllers/AccessCheckerController.cs
+++ IdentityManager/Controllers/AccessCheckerController.cs
@@ -2,18 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IdentityManager.Controllers
 {
+    [Authorize]
     public class AccessCheckerController : Controller
     {
+        [AllowAnonymous]
         //Accessible by everyone, even if users are not logged in.
         public IActionResult AllAccess()
         {
             return View();
         }
 
+        [Authorize]
         //Accessible by logged in users.
         public IActionResult AuthorizedAccess()
         {
